2695dbbea525580d4491d94897f2781bbde8a1b6
t.fouchard <t.fouchard@qwant.com>
2018-10-01 17:41:21 +0200

    fix docker hub ns

--- run_docker.sh
+++ run_docker.sh
@@ -6,6 +6,6 @@ sudo docker run \
   -e CONFIG_PATH='/config_prod.yml' \
   --mount type=bind,source="$(pwd)"/config_prod.yml,target=/config_prod.yml \
   --mount type=bind,source="$(pwd)"/.lego,target=/.lego \
-  hightoxicity/docker-lego
+  djnos/docker-lego
 #  --interactive=true \
 #  --privileged=true 
